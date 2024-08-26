abed7ca9e64a46d161dfeee364e1452f6af62a73
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-26 14:23:38 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -9,7 +9,7 @@ kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
 
 # remove trailing slash on DT_ENDPOINT if it exists
 DT_ENDPOINT=$(echo "$DT_ENDPOINT" | sed "s,/$,,")
-echo "Removed trailing slashes in $DT_ENDPOINT"
+echo "Removed any trailing slashes in DT_ENDPOINT"
 
 # replace the endpoint with user provided value
 # sed -i "s|DT_ENDPOINT|$DT_ENDPOINT|" .devcontainer/dynakube.yaml
