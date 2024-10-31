38758350ac067426e3a75b47ea0748625c11843f
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-10-31 15:03:02 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-if [[ -z $DT_TOKEN || -z $DT_ENDPOINT ]] then
-    echo "Required variables DT_TOKEN and DT_ENDPOINT are not set. Exiting..."
-    exit 1
-fi
-
 kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
 
-# ENV var pre-processing
-# remove trailing slash on DT_ENDPOINT if it exists
-DT_ENDPOINT=$(echo "$DT_ENDPOINT" | sed "s,/$,,")
-echo "Removed any trailing slashes in DT_ENDPOINT"
-# Base64 encode DT_TOKEN, remove newlines that are auto added
-DT_TOKEN=$(echo -n $DT_TOKEN | base64 -w 0)
-
 # install the Dynatrace operator
-helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operator \
-    --create-namespace \
-    --namespace dynatrace \
-    --atomic
-
-# Apply the Dynakube in ApplicationOnly mode
-# using envsubst for env var replacement
-envsubst < .devcontainer/dynakube.yaml | kubectl apply -f -    
+helm repo add datadog https://helm.datadoghq.com
+helm install datadog-operator datadog/datadog-operator
+kubectl create secret generic datadog-secret --from-literal api-key=f72eefd69871937ee5d0394f9694dc87
 
 # deploy microservices without loadgenerator service
 # Note in kustomize/kustomization.yaml the components/remove-loadgen is enabled
